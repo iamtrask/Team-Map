2e9b02ecb497e15a199e36aed4613283cab9b7aa
Amber Trask <amberleightrask@gmail.com>
2018-01-09 13:17:21 +0000

    Dataset Update

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ var map = new mapboxgl.Map({
 map.on('load', function() {
     map.addSource('source-contribute', {
         type: 'vector',
-        url: 'mapbox://4mber.cjbtsetqw1kit2qrt6g2rjjy6-1t32t'
+        url: 'mapbox://4mber.cjc7n2ccr1hzs2xms92tjlb44-3ty71'
     });
 });
 
 // Creates repo buttons to sort layers:
-var toggleableLayerIds = ['Adapters', 'Docs', 'PySonar', 'PySyft', 'Sonar', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-serverless', 'Other Contributions'];
+var toggleableLayerIds = ['Adapters', 'Docs', 'PySonar', 'PySyft', 'Sonar', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-ui', 'ml-agents', 'syft.js', 'Other Contributions'];
 var id = '';
 var link = '';
 
@@ -75,7 +75,7 @@ layers.appendChild(allLink);
 // Creates all markers & pop up windows, including all functionality:
 map.on('click', function(e) {
     var features = map.queryRenderedFeatures(e.point, {
-        layers: ['Other Contributions', 'Adapters', 'Docs', 'PySonar', 'PySyft', 'Sonar', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-serverless']
+        layers: ['Other Contributions', 'Adapters', 'Docs', 'PySonar', 'PySyft', 'Sonar', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-ui', 'ml-agents', 'syft.js']
     });
     if (!features.length) {
         return;
@@ -139,7 +139,7 @@ map.on('click', function(e) {
     if (feature.properties.GitHub.length != 0) {
         var githubID = feature.properties.GitHub;
         var contributions = [];
-        var repos = ['PySyft', 'PySonar', 'Docs', 'Sonar', 'Adapters', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-serverless'];
+        var repos = ['PySyft', 'PySonar', 'Docs', 'Sonar', 'Adapters', 'Team-Map', 'CampX', 'tutorials', 'OpenMined', 'mine-ui', 'openmined-website', 'openmined-ui', 'ml-agents', 'syft.js'];
         repos.forEach(function(repo) {
             var githubcall = 'https://api.github.com/repos/OpenMined/' + repo + '/contributors?client_id=ac576b085ce24d8ce500&client_secret=00110f8b95ee6bac4abd27a36d58d804695a2a19';
             var git = new XMLHttpRequest();
